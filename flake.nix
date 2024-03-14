db436a37917e9fefce595e206da3511c41aa09e4
chwiggy <emilycwilke@gmail.com>
2024-03-14 14:51:09 +0100

    testing py ugrade

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
         default = pkgs.mkShell {
           packages = with pkgs;
             [ python312 jupyter virtualenv ]
-            ++ (with pkgs.python311Packages; [ pip jupyter pandas geopandas numpy matplotlib umap-learn rasterio]);
+            ++ (with pkgs.python312Packages; [ pip jupyter pandas geopandas numpy matplotlib umap-learn rasterio]);
         };
       });
     };
