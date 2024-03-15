db06fc15e24d33f85bdd75cc2a93a0c1423b145c
chwiggy <emilycwilke@gmail.com>
2024-03-15 14:19:36 +0100

    aargh

--- pkgs/mapclassify/flake.nix
+++ pkgs/mapclassify/flake.nix
@@ -3,11 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    pkgs
   };
 
   outputs = { self, nixpkgs }: {
 
-    mapclassify = pkgs.callPackage ./default.nix
+    mapclassify = pkgs.callPackage ./default.nix;
 
   };
 }
