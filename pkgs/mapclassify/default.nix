a4354c329ae57e22aa2764a8bf7cc2c8bd0df57b
chwiggy <emilycwilke@gmail.com>
2024-03-15 13:40:07 +0100

    changed pyproject status

--- pkgs/mapclassify/default.nix
+++ pkgs/mapclassify/default.nix
@@ -7,10 +7,13 @@
 buildPythonPackage rec {
   pname = mapclassify;
   version = "2.6.1";
-  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-z4Q23FnYaVNG/NOrKW3kZCXsqwDWQJbOvnn7Ueyy65M=";
   };
+
+  nativeBuildInputs = [
+    setuptools-scm
+  ];
 }
