e75a423b1d45a29801a268d50849a25a3d331d14
chwiggy <emilycwilke@gmail.com>
2024-03-15 14:20:55 +0100

    hrmm

--- pkgs/mapclassify/flake.nix
+++ pkgs/mapclassify/flake.nix
@@ -1,9 +1,13 @@
 {
+  lib,
+  pkgs
+}:
+
+{
   description = "A very basic flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    pkgs
   };
 
   outputs = { self, nixpkgs }: {
